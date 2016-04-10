07a75f5869596d31c6e1448dc1f3e3edca7abec1
AdamGrzybkowski <agrzybkowski@outlook.com>
2016-04-10 16:46:37 +0200

    fibonacci odwrotnie

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,38 +10,71 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Podaj ile liczb ciągu chcesz uzyskać : ");
+            //Console.WriteLine("Podaj ile liczb ciągu chcesz uzyskać : ");
+            //int number = int.Parse(Console.ReadLine());
+            //int sum=1;
+            //int sum1 = 1;
+            //int sum2 = 1;
+
+
+
+            //for (int i = 1; i<= number; i++)
+            //{
+            //    if( i == 1)
+            //    {
+            //        sum = sum1;
+            //        Console.WriteLine(sum1);
+
+            //    }
+            //    else if (i == 2)
+            //    {
+            //        sum = sum2;
+            //        Console.WriteLine(sum2);
+
+            //    }
+            //    else
+            //    {
+            //        sum = sum1 + sum2;
+            //        sum1 = sum2;
+            //        sum2 = sum;
+            //        Console.WriteLine(sum);
+            //    }
+            //}
+            Console.WriteLine("Podaj ilość liczb ciągu do wyświetlenia: ");
             int number = int.Parse(Console.ReadLine());
-            int sum=1;
+            int[] fibonacci = new int[number];
+            int sum = 0;
             int sum1 = 1;
             int sum2 = 1;
 
-
-
-            for (int i = 1; i<= number; i++)
+            for (int i = 0; i<= number - 1; i++)
             {
-                if( i == 1)
+                if (i == 1)
                 {
-                    sum = sum1;
-                    Console.WriteLine(sum1);
-
+                    sum = 1;
+                
                 }
-                else if (i == 2)
+                else if (i == 2 )
                 {
-                    sum = sum2;
-                    Console.WriteLine(sum2);
-
+                    sum = 1;
                 }
-                else
+             
+                else if (i !=0)
                 {
                     sum = sum1 + sum2;
                     sum1 = sum2;
                     sum2 = sum;
-                    Console.WriteLine(sum);
+
                 }
+                fibonacci[i] = sum;
+
+               
             }
           
-
+            for (int i = fibonacci.Length -1; i >= 0; i--)
+            {
+                Console.Write(fibonacci[i] + " ");
+            }
 
         }
     }
